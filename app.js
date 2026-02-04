3a91b9e54eba0bbf6425c673373f5a8b7d36b42a
Justice <122542069+goob8079@users.noreply.github.com>
2026-02-04 13:05:08 +0900

    setup prisma session store

--- app.js
+++ app.js
@@ -3,9 +3,34 @@ const path = require("node:path");
 const session = require("express-session");
 const passport = require("passport");
 const router = require('./routes/router');
+const { PrismaPg } = require("@prisma/adapter-pg");
+const { PrismaClient } = require("./generated/prisma/client");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 require("dotenv/config");
 
 const app = express();
+
+// prisma sesison store setup
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+app.use(
+    session({
+        cookie: { maxAge: 7 * 24 * 60 * 60 * 1000}, 
+        secret: process.env.COOKIE_SECRET,
+        resave: true,
+        saveUninitialized: true,
+        store: new PrismaSessionStore(
+            prisma,
+            {
+                checkPeriod: 2 * 60 * 1000,
+                dbRecordIdIsSessionId: true,
+                dbRecordIdFunction: undefined,
+            }
+        )
+    })
+);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
