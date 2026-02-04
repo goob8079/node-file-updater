fbb1e356a235aef26710c9c4f3746a9da3b07132
Justice <122542069+goob8079@users.noreply.github.com>
2026-02-04 11:33:37 +0900

    initialize prisma ORM

---
+++ prisma.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("prisma/config");
+require("dotenv/config");
+
+export default defineConfig({
+  schema: "./prisma/schema.prisma",
+  migrations: {
+    path: "./prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
